Correct formula for metrics

PolyllaFace.py
@@ -122,13 +122,13 @@ class PolyllaFace:
             L_max = max(length_edge_a,length_edge_b,length_edge_c)
 
             q = L_max*(length_edge_a + length_edge_b + length_edge_c) / (4 * sqrt(3) * area) # type: ignore
-            aspects.append(self.mesh.face_list[i].area / q)
+            aspects.append(abs(area - (area * q)))
         for i in range(0, self.mesh.n_tetrahedrons):
             a0 = aspects[self.mesh.tetra_list[i].faces[0]]
             a1 = aspects[self.mesh.tetra_list[i].faces[1]]
             a2 = aspects[self.mesh.tetra_list[i].faces[2]]
             a3 = aspects[self.mesh.tetra_list[i].faces[3]]
-            maxFace = max(a0, a1, a2, a3)
+            maxFace = min(a0, a1, a2, a3)
             if maxFace == a0:
                 longest_faces.append(0)
             elif maxFace == a1:
@@ -155,13 +155,13 @@ class PolyllaFace:
             length_edge_c = self.mesh.edge_list[self.mesh.face_list[i].edges[2]].length
 
             ar = .5 * (length_edge_a * length_edge_b * length_edge_c) / (length_edge_a + length_edge_b + length_edge_c)
-            aspects.append(self.mesh.face_list[i].area/ar)
+            aspects.append(abs(self.mesh.face_list[i].area - (self.mesh.face_list[i].area*ar)))
         for i in range(0, self.mesh.n_tetrahedrons):
             a0 = aspects[self.mesh.tetra_list[i].faces[0]]
             a1 = aspects[self.mesh.tetra_list[i].faces[1]]
             a2 = aspects[self.mesh.tetra_list[i].faces[2]]
             a3 = aspects[self.mesh.tetra_list[i].faces[3]]
-            maxFace = max(a0, a1, a2, a3)
+            maxFace = min(a0, a1, a2, a3)
             if maxFace == a0:
                 longest_faces.append(0)
             elif maxFace == a1:
@@ -190,13 +190,13 @@ class PolyllaFace:
             radious = (semiperimeter - length_edge_a) * (semiperimeter - length_edge_b) * (semiperimeter - length_edge_c) / semiperimeter
             ar = radious / L_max
             
-            aspects.append(self.mesh.face_list[i].area/ar)
+            aspects.append(abs(self.mesh.face_list[i].area  - (self.mesh.face_list[i].area * ar)))
         for i in range(0, self.mesh.n_tetrahedrons):
             a0 = aspects[self.mesh.tetra_list[i].faces[0]]
             a1 = aspects[self.mesh.tetra_list[i].faces[1]]
             a2 = aspects[self.mesh.tetra_list[i].faces[2]]
             a3 = aspects[self.mesh.tetra_list[i].faces[3]]
-            maxFace = max(a0, a1, a2, a3)
+            maxFace = min(a0, a1, a2, a3)
             if maxFace == a0:
                 longest_faces.append(0)
             elif maxFace == a1:
@@ -337,7 +337,7 @@ class PolyllaFace:
             av3 = np.array([v3.x, v3.y, v3.z])
             area = np.linalg.norm(np.cross(av2-av1, av3-av1))
             # print(face.i, area)
-            face.area = area
+            face.area = area*0.5
 
     # Esto puede ser un escrito en dos lineas
     def calculate_max_area_faces(self):
@@ -819,7 +819,7 @@ class PolyllaFace:
             nodes = []
             for face in polyhedron.faces:
                 # print(self.mesh.face_list[face].i, self.mesh.face_list[face].area)
-                suma_area+= self.mesh.face_list[face].area*0.5
+                suma_area+= self.mesh.face_list[face].area
             for tetra in polyhedron.tetras:
                     vertex = [self.mesh.tetra_list[tetra].v1, self.mesh.tetra_list[tetra].v2,self.mesh.tetra_list[tetra].v3,self.mesh.tetra_list[tetra].v4]
                     for v in vertex: